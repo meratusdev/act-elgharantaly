ea57df143d26c90548901f8b1fac603e6d0e21a0
zakiego <belummikir@gmail.com>
2023-03-31 16:05:11 +0800

    feat: post page with ssg

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,6 +1,5 @@
 import { Heading } from "@chakra-ui/react";
 import { DocumentRenderer } from "@keystatic/core/renderer";
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import {
   type GetStaticPaths,
   type GetStaticPropsContext,
@@ -10,8 +9,7 @@ import Balancer from "react-wrap-balancer";
 
 import PageWrapper from "~/components/Layout/PageWrapper";
 import { PostHeader } from "~/components/Layout/Post/Header";
-import { appRouter, trpcServerSide } from "~/server/api/root";
-import { trpc } from "~/utils/api";
+import { trpcServerSide } from "~/server/api/root";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const posts = await trpcServerSide.posts.getAllSlug();
@@ -29,32 +27,25 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps = async (
   context: GetStaticPropsContext<{ id: string }>,
 ) => {
-  const ssg = await createProxySSGHelpers({
-    router: appRouter,
-    ctx: {},
-  });
   const id = context.params?.id as string;
-  await ssg.posts.getOnePost.prefetch({ slug: id });
+
+  const data = await trpcServerSide.posts.getOnePost({
+    slug: id,
+  });
 
   return {
     props: {
-      trpcState: ssg.dehydrate(),
+      data,
       id,
     },
-    revalidate: 1,
   };
 };
 
 const PagePosts = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const { id } = props;
-  const { data } = trpc.posts.getOnePost.useQuery({ slug: id });
-
-  if (!data) {
-    return <p>Loading...</p>;
-  }
+  const { id, data } = props;
 
   return (
-    <PageWrapper title={data.title as string}>
+    <PageWrapper title={data.title}>
       <PostHeader slug={props.id} date={data.publishedDate} />
       <Balancer>
         <Heading>{data.title}</Heading>
