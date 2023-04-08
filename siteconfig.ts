955e33fe643a96ee0aca150deffada7322142f8b
zakiego <belummikir@gmail.com>
2023-04-08 16:18:42 +0800

    feat: add siteconfig

---
+++ siteconfig.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+const ConfigSchema = z.object({
+  author: z.object({
+    name: z.string(),
+    avatar: z.string(),
+  }),
+  pagination: z.object({
+    title: z.object({
+      noOfLines: z.number(),
+    }),
+    summary: z.object({
+      noOfLines: z.number(),
+    }),
+  }),
+});
+
+const createConfig = (config: z.infer<typeof ConfigSchema>) => {
+  return ConfigSchema.parse(config);
+};
+
+export const siteConfig = createConfig({
+  author: {
+    name: "ACT El-Gharantaly",
+    avatar: "/avatar.jpg",
+  },
+  pagination: {
+    title: {
+      noOfLines: 3,
+    },
+    summary: {
+      noOfLines: 2,
+    },
+  },
+});
